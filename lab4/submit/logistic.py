a1bd3a4acf0cf04aaf76b159e37972475c622d1a
Milo Knowles <mknowles@mit.edu>
2018-12-05 15:23:06 -0500

    Add submission

---
+++ lab4/submit/logistic.py
@@ -0,0 +1,297 @@
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+import util
+
+USAGE = "%s <test data folder> <spam folder> <ham folder>"
+NUM_EXAMPLES = 100 # specify 0 or less to use all the training examples, or any positive integer to only train with that many
+SHOW_LOSS_PLOT = True # whether or not to show the plot of loss decreasing over time
+
+def extract_features(f, all_words):
+    """
+    Extract features from file for logistic regression. 
+
+    Inputs
+    ------
+    f: Name of file to extract features from.
+
+    all_words : List of all words in the training set of files. 
+
+    Output
+    ------
+    Extracted features. 
+    """
+    words_in_f = set(util.get_words_in_file(f))
+    features = np.zeros(len(all_words))
+
+    # Set entry to 0 or 1 to indicate absence or presence of word i.
+    for i, w in enumerate(all_words):
+        if w in words_in_f:
+            features[i] = 1.0
+
+    return features
+
+### UNCOMMENT THIS FUNCTION TO OVERRIDE WITH WORD FREQUENCY FEATURES.
+# def extract_features(f, all_words):
+#     """
+#     Each feature is the number of occurrences of a word in the file, divided by
+#     the total number of words in the file.
+#     """
+#     words_in_f = util.get_words_in_file(f)
+#     freq = {}
+
+#     for w in words_in_f:
+#         if w not in freq: freq[w] = 0
+#         freq[w] += 1.0
+
+#     features = np.zeros(len(all_words))
+
+#     # Set entry to 0 or 1 to indicate absence or presence of word i.
+#     for i, w in enumerate(all_words):
+#         if w in freq:
+#             features[i] = freq[w] / float(len(words_in_f))
+
+#     # print(features)
+#     return features
+
+def logistic_eval(y, c, theta):
+    """
+    Compute the cost function for a collection of data points.
+
+    Note that this is just the sum of the cost for each individual data point.
+
+    Inputs
+    ------
+    y: numpy array of features for the data, of shape (num_examples, num_features)
+    
+    c: numpy array of true class labels, either 0 or 1, of shape (num_examples)
+
+    theta: numpy array of model parameters, theta_0 to theta_n, or shape (num_features + 1)
+
+    Output
+    ------
+    logistic regression loss for model prediction compared to true class labels
+    """
+    p_spam = 1.0 / (1.0 + np.exp(-1 * (theta[0] + np.matmul(y, theta[1:]))))
+    p_ham = 1.0 - p_spam
+
+    # Take the first loss term if spam, o.w take ham loss.
+    phi = np.select([c == 1, c == 0], [np.log(p_spam), np.log(p_ham)])
+    return np.sum(phi)
+
+def logistic_derivative(y, c, theta):
+    """
+    Compute the derivative of the cost function with respect to model parameters for a collection of data points.
+
+    Note that this is just the sum of those derivatives for each data point individually. 
+
+    Inputs
+    ------
+    y: numpy array of features for the data, of shape (num_examples, num_features)
+    
+    c: numpy array of true class labels, either 0 or 1, of shape (num_examples)
+
+    theta: numpy array of model parameters, theta_0 to theta_n, or shape (num_features + 1)
+
+
+    Output
+    ------
+    Numpy array G of shape (num_features + 1), where G[i] contains the derivative of the cost function
+    with respect to theta[i]. 
+    """
+    k, n = y.shape
+
+    y_with_bias = np.ones((k, n + 1)) # Add an additional bias feature of all 1s.
+    y_with_bias[:,1:] = y # Add the rest of y features to the right of the col of 1s.
+
+    # Sigmoid activation of all data points (k x 1)
+    sigmoid_y = 1.0 / (1.0 + np.exp(-1 * np.matmul(y_with_bias, theta)))
+
+    # (n+1 x 1)
+    cost_wrt_theta = np.matmul(y_with_bias.T, c) - np.matmul(y_with_bias.T, sigmoid_y)
+    return -1 * cost_wrt_theta
+
+def train_logistic(file_lists_by_category):
+    """
+    Extract features and labels for the logistic regression model, and train the model.
+
+    Note that you'll need to arbitrarily pick one of spam and ham to be 0, and the other to be 1, 
+    when creating the labels for logistic regression. 
+    The choice doesn't matter; just make sure you are consistent about it. 
+
+    Inputs
+    ------
+    A two-element list. The first element is a list of spam files, 
+    and the second element is a list of ham (non-spam) files.
+
+    Output
+    ------
+    Tuple of (theta, all_words) where theta is the vector of trained logistic regression parameters, 
+    and all_words is the list of all words found in the dataset (reused later to make
+    sure we extract features in a consistent manner)
+    """
+    # Build the set of all words.
+    all_words = set()
+    for filelist in file_lists_by_category:
+        for f in filelist:
+            words_in_f = util.get_words_in_file(f)
+            all_words.update(words_in_f)
+    all_words = list(all_words)
+
+    num_spam = len(file_lists_by_category[0])
+    num_ham = len(file_lists_by_category[1])
+
+    num_examples = (num_spam + num_ham)
+    num_features = len(all_words)
+
+    # Allocate data containers.
+    y = np.zeros((num_examples, num_features))
+
+    # Let label spam = 1.
+    c = np.zeros(num_examples)
+    c[:num_spam] = 1.0
+
+    # Get features for each file.
+    j = 0 # Counter.
+    for filelist in file_lists_by_category:
+        for f in filelist:
+            y[j,:] = extract_features(f, all_words)
+            j += 1
+
+    # Optimize parameters.
+    theta = optimize_theta(y, c) #, learning_rate=4.0, convergence_threshold=1e-6)
+    return theta, all_words
+
+def classify_message(filename, theta, all_words):
+    """
+    Predict the label of a file, by just checking whether the logistic regression
+    model outputs a prediction that is more or less than 0.5. 
+
+    Inputs
+    ------
+    filename: Name of file to predict spam or ham on.
+
+    theta: trained logistic regression parameters
+
+    all_words: list of all words returned by train_logistic(). 
+
+    Output
+    ------
+    'spam' or 'ham'
+    """
+    features = extract_features(filename, all_words)
+
+    # Evaluate p_spam.
+    p_spam = 1.0 / (1.0 + np.exp(-1 * (theta[0] + np.matmul(features, theta[1:]))))
+
+    if p_spam >= 0.5:
+        return 'spam'
+    else:
+        return 'ham'
+
+"""
+
+END OF YOUR CODE
+
+DO NOT MODIFY ANY OF THE CODE THAT FOLLOWS.
+
+"""
+
+def optimize_theta(y, c, show_loss_plot=SHOW_LOSS_PLOT, learning_rate=0.5, convergence_threshold=1e-3):
+    """
+    Train the logistic regression model. 
+    DO NOT MODIFY.
+
+    Inputs
+    ------
+    y: numpy array of features for the data, of shape (num_examples, num_features)
+    
+    c: numpy array of true class labels, either 0 or 1, of shape (num_examples)
+
+    show_loss_plot: whether or not to show the plot of logistic regression cost function over time.
+
+    learning_rate: parameter controlling size of parameter update steps. the default should work fine.
+
+    convergence_threshold: parameter controlling when we consider the model 'done training.' the default should work fine.
+
+    Output
+    -------
+    Numpy array theta containing trained logistic model parameters theta_0, ... theta_n, of shape (num_features + 1)
+    """
+    num_examples, num_features = y.shape[0], y.shape[1]
+    theta = np.array([0 for _ in range(num_features + 1)])
+
+    prev_loss = float('inf')
+    loss = -logistic_eval(y, c, theta)
+    print('Initial loss:', loss)
+    losses = [loss]
+
+    ctr = 0
+
+    while loss < prev_loss - convergence_threshold * num_examples: # scale by num_examples as a normalization
+        gradient = logistic_derivative(y, c, theta) / num_examples # scale by num_examples as a normalization
+        theta = theta - learning_rate * gradient
+        prev_loss = loss
+        loss = -logistic_eval(y, c, theta)
+        losses.append(loss)
+
+        if ctr % 100 == 0:
+            print('Loss (t=%d): %f' % (ctr, loss))
+        ctr += 1
+
+    if show_loss_plot:
+        plt.plot(range(len(losses)), losses)
+        plt.xlabel('Gradient Descent Iterations')
+        plt.ylabel('Logistic Regression Cost')
+        plt.show()
+    
+    return theta
+
+if __name__ == '__main__':
+    ### Read arguments
+    if len(sys.argv) != 4:
+        print(USAGE % sys.argv[0])
+    testing_folder = sys.argv[1]
+    (spam_folder, ham_folder) = sys.argv[2:4]
+    file_lists = []
+    for folder in (spam_folder, ham_folder):
+        if NUM_EXAMPLES > 0:
+            file_lists.append(util.get_files_in_folder(folder)[:NUM_EXAMPLES])
+        else:
+            file_lists.append(util.get_files_in_folder(folder))
+    print("Extracting Features and Training...")
+    theta, all_words = train_logistic(file_lists)
+
+    # # Here, columns and rows are indexed by 0 = 'spam' and 1 = 'ham'
+    # # rows correspond to true label, columns correspond to guessed label
+    performance_measures = np.zeros([2,2])
+
+    ### Classify and measure performance
+    print("Testing...")
+    idx = 1
+    for filename in (util.get_files_in_folder(testing_folder)):
+        idx += 1
+        ## Classify
+        label = classify_message(filename, theta, all_words)
+        ## Measure performance
+        # Use the filename to determine the true label
+        base = os.path.basename(filename)
+        true_index = ('ham' in base)
+        guessed_index = (label == 'ham')
+        performance_measures[int(true_index), int(guessed_index)] += 1
+
+        # Uncomment this line to see which files your classifier
+        # gets right/wrong:
+        # print("%s : %s" %(label, filename))
+
+    template="You correctly classified %d out of %d spam messages, and %d out of %d ham messages."
+    # Correct counts are on the diagonal
+    correct = np.diag(performance_measures)
+    # totals are obtained by summing across guessed labels
+    totals = np.sum(performance_measures, 1)
+    print(template % (correct[0],
+                      totals[0],
+                      correct[1],
+                      totals[1]))
---
+++ lab4/submit/naivebayes.py
@@ -0,0 +1,197 @@
+from __future__ import division
+import sys
+import os.path
+import numpy as np
+
+import util
+
+USAGE = "%s <test data folder> <spam folder> <ham folder>"
+
+def get_counts(file_list):
+    """
+    Computes counts for each word that occurs in the files in file_list.
+
+    Inputs
+    ------
+    file_list : a list of filenames, suitable for use with open() or 
+                util.get_words_in_file()
+
+    Output
+    ------
+    A dict whose keys are words, and whose values are the number of files the
+    key occurred in.
+    """
+    ctr = util.Counter()
+    for f in file_list:
+        for w in set(util.get_words_in_file(f)):
+            ctr[w] += 1
+    return ctr
+
+def get_log_probabilities(file_list):
+    """
+    Computes log-probabilities for each word that occurs in the files in 
+    file_list.
+
+    Input
+    -----
+    file_list : a list of filenames, suitable for use with open() or 
+                util.get_words_in_file()
+
+    Output
+    ------
+    A dict whose keys are words, and whose values are the log of the smoothed
+    estimate of the fraction of files the key occurred in.
+
+    Hint
+    ----
+    The data structure util.DefaultDict will be useful to you here, as will the
+    get_counts() helper above.
+    """
+    ctr = get_counts(file_list)
+    K = len(file_list) # Number of documents.
+
+    # Due to smoothing, the default probability for a word that does not exist
+    # is 1 / K+2.
+    default = 1.0 / (K + 2)
+    prob = util.DefaultDict(lambda: np.log(np.array([1-default, default])))
+
+    for word in ctr:
+        p_yes = float(1 + ctr[word]) / (K + 2)
+        p_no  = 1 - p_yes
+        assert(p_yes > 0 and p_yes < 1)
+        assert(p_no > 0 and p_no < 1)
+        prob[word] = np.log(np.array([p_no, p_yes]))
+
+    return prob
+
+def learn_distributions(file_lists_by_category):
+    """
+    Input
+    -----
+    A two-element list. The first element is a list of spam files, 
+    and the second element is a list of ham (non-spam) files.
+
+    Output
+    ------
+    (log_probabilities_by_category, log_prior)
+
+    log_probabilities_by_category : A list whose first element is a smoothed
+                                    estimate for log P(y=w_j|c=spam) (as a dict,
+                                    just as in get_log_probabilities above), and
+                                    whose second element is the same for c=ham.
+
+    log_prior_by_category : A list of estimates for the log-probabilities for
+                            each class:
+                            [est. for log P(c=spam), est. for log P(c=ham)]
+    """
+    spam_files, ham_files = file_lists_by_category
+    K_spam, K_ham = len(spam_files), len(ham_files)
+    K = K_spam + K_ham
+    print(K_spam, K_ham)
+
+    p_wj_given_spam = get_log_probabilities(spam_files)
+    p_wj_given_ham = get_log_probabilities(ham_files)
+
+    prior_spam = np.log(float(K_spam) / K)
+    prior_ham = np.log(float(K_ham) / K)
+    # p_spam = 0.999
+    # prior_spam = np.log(p_spam)
+    # prior_ham = np.log(1 - p_spam)
+
+    print(prior_spam, prior_ham)
+
+    return ([p_wj_given_spam, p_wj_given_ham], [prior_spam, prior_ham])
+
+def classify_message(message_filename,
+                     log_probabilities_by_category,
+                     log_prior_by_category,
+                     names = ['spam', 'ham']):
+    """
+    Uses Naive Bayes classification to classify the message in the given file.
+
+    Inputs
+    ------
+    message_filename : name of the file containing the message to be classified
+
+    log_probabilities_by_category : See output of learn_distributions
+
+    log_prior_by_category : See output of learn_distributions
+
+    names : labels for each class (for this problem set, will always be just 
+            spam and ham).
+
+    Output
+    ------
+    One of the labels in names.
+    """
+    words = util.get_words_in_file(message_filename)
+    words_set = set(words) # Faster lookup.
+
+    # Start with just the prior probability of 'spam' and 'ham', respectively.
+    posteriors = np.array(log_prior_by_category)
+    # print('Prior:', posteriors)
+
+    # Feature space is all the words from spam and ham.
+    all_words = set(log_probabilities_by_category[0].keys())
+    all_words.update(list(log_probabilities_by_category[1].keys()))
+    # print(len(all_words))
+
+    # Sum log-probabilities of generating each observed word given the label.
+    # for w in log_probabilities_by_category[0]:
+    for w in all_words:
+        has_word = 1 if w in words_set else 0
+        posteriors[0] += log_probabilities_by_category[0][w][has_word]
+        posteriors[1] += log_probabilities_by_category[1][w][has_word]
+
+    # print(posteriors)
+    if posteriors[0] >= posteriors[1]:
+        return names[0]
+    else:
+        return names[1]
+
+if __name__ == '__main__':
+    ### Read arguments
+    if len(sys.argv) != 4:
+        print(USAGE % sys.argv[0])
+    testing_folder = sys.argv[1]
+    (spam_folder, ham_folder) = sys.argv[2:4]
+
+    ### Learn the distributions
+    file_lists = []
+    for folder in (spam_folder, ham_folder):
+        file_lists.append(util.get_files_in_folder(folder))
+    (log_probabilities_by_category, log_priors_by_category) = \
+            learn_distributions(file_lists)
+
+    # Here, columns and rows are indexed by 0 = 'spam' and 1 = 'ham'
+    # rows correspond to true label, columns correspond to guessed label
+    performance_measures = np.zeros([2,2])
+
+    ### Classify and measure performance
+    for filename in (util.get_files_in_folder(testing_folder)):
+        ## Classify
+        label = classify_message(filename,
+                                 log_probabilities_by_category,
+                                 log_priors_by_category,
+                                 ['spam', 'ham'])
+        ## Measure performance
+        # Use the filename to determine the true label
+        base = os.path.basename(filename)
+        true_index = ('ham' in base)
+        guessed_index = (label == 'ham')
+        performance_measures[int(true_index), int(guessed_index)] += 1
+
+        # Uncomment this line to see which files your classifier
+        # gets right/wrong:
+        # print("%s : %s" %(label, filename))
+
+    template="You correctly classified %d out of %d spam messages, and %d out of %d ham messages."
+    # Correct counts are on the diagonal
+    correct = np.diag(performance_measures)
+    # totals are obtained by summing across guessed labels
+    totals = np.sum(performance_measures, 1)
+    print(template % (correct[0],
+                      totals[0],
+                      correct[1],
+                      totals[1]))
+
